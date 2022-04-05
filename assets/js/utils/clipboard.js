d92f3d9294fef6369cc1c2962e3917189dbf5dd4
Miłosz Skaza <milosz.skaza@ctfd.io>
2022-04-05 15:53:32 +0000

    Bootstrap 5

--- assets/js/utils/clipboard.js
+++ assets/js/utils/clipboard.js
@@ -1,16 +1,15 @@
 import { Tooltip } from "bootstrap";
 
 export function copyToClipboard($input) {
-  // Copy to clipboard
-  navigator.clipboard.writeText($input.value);
-
   const tooltip = new Tooltip($input, {
     title: "Copied!",
     trigger: "manual",
-  });
+  })
 
-  tooltip.show();
-  setTimeout(() => {
-    tooltip.hide();
-  }, 1500);
+  navigator.clipboard.writeText($input.value).then(() => {
+      tooltip.show();
+      setTimeout(() => {
+        tooltip.hide();
+      }, 1500);
+  });
 }
